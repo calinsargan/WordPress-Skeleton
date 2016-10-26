ab11a7e62280619ea6d3e0c10ed989d8aa2e3daf
Calin Sargan <calin.sargan@gmail.com>
2016-10-26 10:40:41 +0300

    gulp additions

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
 
 var theme = 'content/themes/[theme-name]';
 
@@ -9,17 +11,23 @@ var paths = {
     bower: theme + '/components',
 }
 
-gulp.task('sass', function(){
-  return gulp.src(paths.theme + '/scss/style.scss')
+gulp.task('sass', function(done){
+  gulp.src(paths.theme + '/scss/style.scss')
     .pipe(sourcemaps.init())
     .pipe(sass({
         includePaths: [
             paths.bower + '/bootstrap-sass/assets/stylesheets',
             paths.bower + '/font-awesome/scss'
         ]
-    }))
+    }).on('error', sass.logError))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.theme + '/css'))
+    .pipe(minifyCss({
+      keepSpecialComments: 0
+    }))
+    .pipe(rename({ extname: '.min.css' }))
+    .pipe(gulp.dest(paths.theme + '/css'))
+    .on('end', done); 
 });
 
  gulp.task('watch', function() {
